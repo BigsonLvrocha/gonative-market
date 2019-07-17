8cbb0192dd74b0ae98dd88ad2b679b1ad1c81b3e
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-07-17 20:56:39 -0300

    adds initial styling of details without image

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '~/components/Header';
 import Bottom from '~/components/BottomNavigation';
-import { Container } from './styles';
+import {
+  Container,
+  ContentCard,
+  ContentContainer,
+  TextContainer,
+  DescriptionContainer,
+  Title,
+  Brand,
+  Price,
+  AddToCartButtom,
+  AddToCartButtonText,
+} from './styles';
 
-const Details = () => (
-  <Container>
-    <Header title="Detalhes do produto" backButton />
-    <Bottom />
-  </Container>
-);
+const Details = ({ navigation }) => {
+  const product = navigation.getParam('product');
+  return (
+    <Container>
+      <Header title="Detalhes do produto" backButton />
+      <ContentContainer>
+        <ContentCard>
+          <TextContainer>
+            <DescriptionContainer>
+              <Title>{product.name}</Title>
+              <Brand>{product.brand}</Brand>
+            </DescriptionContainer>
+            <Price>{product.price}</Price>
+          </TextContainer>
+          <AddToCartButtom>
+            <AddToCartButtonText>Adicionar ao carrinho</AddToCartButtonText>
+          </AddToCartButtom>
+        </ContentCard>
+      </ContentContainer>
+      <Bottom />
+    </Container>
+  );
+};
+
+Details.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
 
 export default Details;
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,8 +1,64 @@
 import styled from 'styled-components/native';
+import { colors, metrics } from '~/styles';
 
 export const Container = styled.View`
   flex-direction: column;
   margin: 0;
   padding: 0;
   flex: 1;
+`;
+
+
+export const ContentContainer = styled.View`
+  background: ${colors.lighter};
+  align-items: stretch;
+  flex: 1;
+`;
+
+export const ContentCard = styled.View`
+  margin: ${metrics.baseMargin}px;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  padding: ${metrics.basePadding}px;
+  border-radius: ${metrics.baseRadius}px;
+  background: ${colors.white};
+`;
+
+export const TextContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const DescriptionContainer = styled.View`
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const Title = styled.Text`
+  color: ${colors.black};
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+export const Brand = styled.Text`
+  color: ${colors.light};
+`;
+
+export const Price = styled.Text`
+  color: ${colors.secundary};
+  font-size: 36px;
+  font-weight: bold;
+`;
+
+export const AddToCartButtom = styled.TouchableOpacity`
+  padding: ${metrics.basePadding}px;
+  background: ${colors.secundary};
+  margin-top: ${metrics.baseMargin}px;
+`;
+
+export const AddToCartButtonText = styled.Text`
+  color: ${colors.white};
+  text-align: center;
 `;
